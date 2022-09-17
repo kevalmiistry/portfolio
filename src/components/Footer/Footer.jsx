ecafd3f723a4260cd442d07b30eb28d36d7871e0
Keval <kevalpm@gmail.com>
2022-09-17 20:59:08 +0530

    socials and email added with mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,6 @@
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Footer.scss';
 
@@ -5,7 +8,16 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer_main">
-                &copy; {new Date().getFullYear()} Keval Mistry
+                <div className="socials">
+                    <a target='_blank' rel="noreferrer" href="https://github.com/kevalmiistry/"><FontAwesomeIcon icon={faGithub} /> </a>
+                    <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/keval-mistry-499534161/"><FontAwesomeIcon icon={faLinkedin} /> </a>
+                    <a target='_blank' rel="noreferrer" href="https://www.instagram.com/kevalmiistry/"><FontAwesomeIcon icon={faInstagram} /> </a>
+                    <a target='_blank' rel="noreferrer" href="https://twitter.com/kevalmiistry/"><FontAwesomeIcon icon={faTwitter} /> </a>
+                    <a className='mobile_email' href="mailto:kevalpm@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> </a>
+                </div>
+                <p>&copy; {new Date().getFullYear()} Keval Mistry</p>
+                {/* <div ></div> */}
+                <div className="email"><a href="mailto:kevalpm@gmail.com">kevalpm@gmail.com</a></div>
             </div>
         </footer>
     );
