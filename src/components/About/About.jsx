b0409d1a8083fb5b633198ade2db7959668bd603
Keval <kevalpm@gmail.com>
2022-12-05 15:20:57 +0530

    Nav bar refactored with motion

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { faHandPeace } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import S from './About.module.scss';
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import {
     faHandPointer
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import './Navbar.scss';
+import S from './Navbar.module.scss';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Navbar = () => {
     let Location = useLocation();
@@ -14,67 +14,131 @@ const Navbar = () => {
         about: 'About',
         education: 'Education',
         projects: 'Projects'
-
     };
     let t = undefined;
     t = Location.pathname.slice(1);
 
     return (
         <>
-            <nav id="navbar_main">
-                <div className="navbar_logo">
-                    KM{' '}
-                    <span className="small_text">
-                        <FontAwesomeIcon icon={faArrowRightLong} />{' '}
-                        {t ? options[t]:'About'}
-                    </span>
+            <nav className={S.navbar_main}>
+                <div className={S.navbar_logo}>
+                    <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className={S.logo}
+                    >
+                        KM
+                        <FontAwesomeIcon
+                            className={S.arrow_icon}
+                            icon={faArrowRightLong}
+                        />
+                    </motion.p>
+                    <div className={S.small_text_wrapper}>
+                        {t ? (
+                            options[t] && (
+                                <>
+                                    <motion.div
+                                        initial={{ translateX: '-150%' }}
+                                        animate={{ translateX: '0%' }}
+                                        exit={{ translateX: '100%' }}
+                                        transition={{ delay: 0.35 }}
+                                        className={S.small_text}
+                                    >
+                                        {options[t]}
+                                    </motion.div>
+                                </>
+                            )
+                        ) : (
+                            <>
+                                <motion.div
+                                    initial={{ translateX: '-150%' }}
+                                    animate={{ translateX: '0%' }}
+                                    exit={{ translateX: '100%' }}
+                                    transition={{ duration: 0.5, delay: 0.35 }}
+                                    className={S.small_text}
+                                >
+                                    {'About'}
+                                </motion.div>
+                            </>
+                        )}
+                    </div>
                 </div>
-                <div className="menu_section">
+                <div className={S.menu_section}>
                     <ul>
-                        <li>
-                            <span className="menu_initial">M</span>
+                        <motion.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ delay: 0.3 }}
+                        >
+                            <span className={S.menu_initial}>M</span>
                             <NavLink
                                 to="/about"
-                                className={`menu_main_txt ${Location.pathname==='/'?'active':''}`}
+                                className={`${S.menu_main_txt} ${
+                                    Location.pathname === '/' ? 'active' : ''
+                                }`}
                                 activeclassname="active"
                             >
                                 About
                             </NavLink>
-                        </li>
-                        <li>
-                            <span className="menu_initial">E</span>
+                        </motion.li>
+                        <motion.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ delay: 0.4 }}
+                        >
+                            <span className={S.menu_initial}>E</span>
                             <NavLink
                                 to="/technologies"
-                                className="menu_main_txt"
+                                className={S.menu_main_txt}
                                 activeclassname="active"
                             >
                                 Techs
                             </NavLink>
-                        </li>
-                        <li>
-                            <span className="menu_initial">N</span>
+                        </motion.li>
+                        <motion.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ delay: 0.5 }}
+                        >
+                            <span className={S.menu_initial}>N</span>
                             <NavLink
                                 to="/projects"
-                                className="menu_main_txt"
+                                className={S.menu_main_txt}
                                 activeclassname="active"
                             >
                                 Project
                             </NavLink>
-                        </li>
-                        <li>
-                            <span className="menu_initial">U</span>
+                        </motion.li>
+                        <motion.li
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ delay: 0.6 }}
+                        >
+                            <span className={S.menu_initial}>U</span>
                             <NavLink
                                 to="/education"
-                                className="menu_main_txt"
+                                className={S.menu_main_txt}
                                 activeclassname="active"
                             >
                                 Education
                             </NavLink>
-                        </li>
-                        <FontAwesomeIcon
-                            icon={faHandPointer}
-                            className="mobile_nav_point"
-                        />
+                        </motion.li>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ delay: 1.1 }}
+                        >
+                            <FontAwesomeIcon
+                                icon={faHandPointer}
+                                className={S.mobile_nav_point}
+                            />
+                        </motion.div>
                     </ul>
                 </div>
             </nav>
